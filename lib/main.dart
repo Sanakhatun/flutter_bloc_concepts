5d314984f97da3a9376d1a02013745697547262e
Sanakhatun Shaikh <sanakhatun.shaikh@mobicule.com>
2022-09-28 11:36:24 +0530

    Optimized way to add builder & listener in the single Widget: Implementation of BlocConsumer

--- lib/main.dart
+++ lib/main.dart
@@ -39,67 +39,71 @@ class _MyHomePageState extends State<MyHomePage> {
   /// BlocBuilder: This builder function may be called Multiple Times Per State(due to Flutter Engine)
   /// BlocListener: This listener function is called only once per state(Not including the initial state)
   ///
+  ///
+  /// BlocBuilder:Re-builds the UI for every new state coming form the bloc
+  ///
+  ///
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: BlocListener<CounterCubit, CounterState>(
-          listener: (context, state) {
-            if (state.wasIncremented == true) {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text("Increment button pressed")));
-            } else {
-              ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text("Decrement button pressed")));
-            }
-          },
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'You have pushed the button this many times:',
-                ),
-                //BlocBuilder:Re-builds the UI for every new state coming form the bloc
-                BlocBuilder<CounterCubit, CounterState>(
-                  builder: (context, state) {
-                    if (state.counterValue > 0) {
-                      return Text(
-                        'POSITIVE NUMBER: ${state.counterValue.toString()}',
-                        style: Theme.of(context).textTheme.headline4,
-                      );
-                    } else {
-                      return Text(
-                        'NEGATIVE NUMBER: ${state.counterValue.toString()}',
-                        style: Theme.of(context).textTheme.headline4,
-                      );
-                    }
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'You have pushed the button this many times:',
+              ),
+              //
+              BlocConsumer<CounterCubit, CounterState>(
+                listener: (context, state) {
+                  if (state.wasIncremented == true) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Increment button pressed")));
+                  } else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text("Decrement button pressed")));
+                  }
+                },
+                builder: (context, state) {
+                  return BlocBuilder<CounterCubit, CounterState>(
+                    builder: (context, state) {
+                      if (state.counterValue > 0) {
+                        return Text(
+                          'POSITIVE NUMBER: ${state.counterValue.toString()}',
+                          style: Theme.of(context).textTheme.headline4,
+                        );
+                      } else {
+                        return Text(
+                          'NEGATIVE NUMBER: ${state.counterValue.toString()}',
+                          style: Theme.of(context).textTheme.headline4,
+                        );
+                      }
+                    },
+                  );
+                },
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    /*BlocProvider: Creates & Provides the only instance of a bloc to the subtree*/
+                    BlocProvider.of<CounterCubit>(context).increment();
                   },
+                  tooltip: '+',
+                  child: const Icon(Icons.add),
                 ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      FloatingActionButton(
-                        onPressed: () {
-                          /*BlocProvider: Creates & Provides the only instance of a bloc to the subtree*/
-                          BlocProvider.of<CounterCubit>(context).increment();
-                        },
-                        tooltip: '+',
-                        child: const Icon(Icons.add),
-                      ),
-                      FloatingActionButton(
-                        onPressed: () {
-                          BlocProvider.of<CounterCubit>(context).decrement();
-                        },
-                        tooltip: '-',
-                        child: const Icon(Icons.remove),
-                      ),
-                    ]),
-              ],
-            ),
-          )),
-    );
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  tooltip: '-',
+                  child: const Icon(Icons.remove),
+                ),
+              ]),
+            ],
+          ),
+        ));
   }
 }
